e4733f94a66dc86dc3194b8f16adcad3531edced
Samuel782 <samuelluggeri@gmail.com>
2024-05-18 09:51:57 +0200

    Commit01

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ function convertToDecimal() {
     const decimal = parseInt(binary, 2).toString(10);
     document.getElementById("decimalOutput").textContent = `Decimale: ${decimal}`;
 }
+
+function convertToBinary() {
+    const decimal = document.getElementById("decimalInput").value;
+    if (decimal === "" || isNaN(decimal) || parseInt(decimal) < 0) {
+        alert("Per favore, inserisci un numero decimale valido (positivo).");
+        return;
+    }
+
+    let numeroDecimale = parseInt(decimal);
+    let numeroBinario = "";
+
+    if (numeroDecimale === 0) {
+        numeroBinario = "0";
+    } else {
+        while (numeroDecimale > 0) {
+            let resto = numeroDecimale % 2;
+            numeroBinario = resto + numeroBinario;
+            numeroDecimale = Math.floor(numeroDecimale / 2);
+        }
+    }
+
+    document.getElementById("binaryOutput").textContent = `Binario: ${numeroBinario}`;
+}
